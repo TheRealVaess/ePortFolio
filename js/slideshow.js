9ec5c65fa4afbccfd955d0b72fd802e1624ee030
Veisseyre <benvei@sfr.fr>
2018-04-22 11:13:01 +0200

    Changement complet du portfolio

---
+++ js/slideshow.js
@@ -0,0 +1,9 @@
+$('.slideshow').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    pauseOnHover: true
+});
