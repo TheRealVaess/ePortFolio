dac04bcd7a1f9571ef4524635d3c6c3cddfed0b0
Veisseyre <benvei@sfr.fr>
2019-05-19 23:04:24 +0200

    CSS test neon

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -39,32 +39,68 @@ export default {
       justify-content: center;
       align-items: center;
       height: 100%;
-      width: 8%;
+      width: 6%;
       transform: skew(10deg, 0deg);
       writing-mode: vertical-rl;
       text-orientation: upright;
-      margin: 0 2%;
+      margin: 0 4%;
+      transition: 0.5s;
+      background-color: white;
     }
 
     li:hover {
       transform: skew(10deg, 0deg) scale(1.5);
       z-index: 10;
+      transition: 0.3s;
     }
 
     li:nth-child(1) {
-      background-color: red;
+      -webkit-animation: neon-red 1.5s ease-in-out infinite alternate;
+      -moz-animation: neon-red 1.5s ease-in-out infinite alternate;
+      animation: neon-red 1.5s ease-in-out infinite alternate;
     }
 
     li:nth-child(2) {
-      background-color: cyan;
+      -webkit-animation: neon-blue 1.5s ease-in-out infinite alternate;
+      -moz-animation: neon-blue 1.5s ease-in-out infinite alternate;
+      animation: neon-blue 1.5s ease-in-out infinite alternate;
     }
 
     li:nth-child(3) {
-      background-color: green;
+      -webkit-animation: neon-green 1.5s ease-in-out infinite alternate;
+      -moz-animation: neon-green 1.5s ease-in-out infinite alternate;
+      animation: neon-green 1.5s ease-in-out infinite alternate;
     }
 
     li:nth-child(4) {
       background-color: yellow;
+    }
+  }
+
+  @-webkit-keyframes neon-red {
+    from {
+      box-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px red, 0 0 50px red, 0 0 60px red, 0 0 70px red, 0 0 80px red;
+    }
+    to {
+      box-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px red, 0 0 35px red, 0 0 40px red, 0 0 50px red, 0 0 75px red;
+    }
+  }
+
+  @-webkit-keyframes neon-blue {
+    from {
+      box-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px cyan, 0 0 50px cyan, 0 0 60px cyan, 0 0 70px cyan, 0 0 80px cyan;
+    }
+    to {
+      box-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px cyan, 0 0 35px cyan, 0 0 40px cyan, 0 0 50px cyan, 0 0 75px cyan;
+    }
+  }
+
+  @-webkit-keyframes neon-green {
+    from {
+      box-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px green, 0 0 50px green, 0 0 60px green, 0 0 70px green, 0 0 80px green;
+    }
+    to {
+      box-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px green, 0 0 35px green, 0 0 40px green, 0 0 50px green, 0 0 75px green;
     }
   }
 </style>
